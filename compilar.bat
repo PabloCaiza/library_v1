a8c0dae244bbaded74dd7254e2dd72da9a6f764c
pablo <pablocuri2015@hotmail.com>
2023-01-29 10:16:54 -0500

    deplay

--- compilar.bat
+++ compilar.bat
@@ -1,19 +1,5 @@
-git clone https://github.com/PabloCaiza/library_v1.git
-cd ./library_v1
-cd ./author-microservice
-./gradlew build -Dquarkus.package.type=uber-jar
-docker build -t pjcaiza/app-authors:1.0 .
-cd ..
-cd ./book-microservice
-gradlew jar && gradlew copyLibs && docker build -t pjcaiza/app-books:1.0 .
-cd ..
-cd ./book-client
-gradlew jar && gradlew copyLibs && docker build -t pjcaiza/app-web:1.0 . .
-docker push pjcaiza/app-web:1.0 
-docker push pjcaiza/app-books:1.0
-docker push pjcaiza/app-authors:1.0
-cd ..
-docker compose up
+git clone https://github.com/PabloCaiza/library_v1.git && cd ./library_v1 && cd ./author-microservice && gradlew build -Dquarkus.package.type=uber-jar && docker build -t pjcaiza/app-authors:1.0 . && cd .. && cd ./book-microservice && gradlew jar && gradlew copyLibs && docker build -t pjcaiza/app-books:1.0 . && cd .. && cd ./book-client && gradlew jar && gradlew copyLibs && docker build -t pjcaiza/app-web:1.0 . && docker push pjcaiza/app-web:1.0 && docker push pjcaiza/app-books:1.0 && docker push pjcaiza/app-authors:1.0 && cd .. && docker compose up
+
 
 
 
